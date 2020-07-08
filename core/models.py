75151c9e9d83f8eabf944360d2fa54ec7f22e472
Cristiano <webnowone@gmail.com>
2020-07-08 00:06:40 -0300

    Criado tabela de eventos para agenda.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    """ Exigindo que a tabela se chame evento """
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
